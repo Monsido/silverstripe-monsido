16ffe94357f6e6eaf9bcf8339f686fd959098dd9
Henrik Olsen <henrik@mouseketeers.dk>
2019-06-20 13:47:49 +0200

    Fixed link to front end page

--- code/MonsidoSiteTreeExtension.php
+++ code/MonsidoSiteTreeExtension.php
@@ -9,7 +9,7 @@ use SilverStripe\Control\Director;
 
 class MonsidoSiteTreeExtension extends extension {
 	public function updateCMSFields(FieldList $fields) {
-		$siteURL = ($this->owner->URLSegment != 'home') ? Director::absoluteURL($this->owner->URLSegment) : Director::absoluteBaseURL();
+		$siteURL = ($this->owner->URLSegment != 'home') ? Director::absoluteURL(rtrim($this->owner->Link(), '/')) : Director::absoluteBaseURL();
 		$fields->push(new LiteralField('MonsidoURLComment', "<!-- Monsido: public_urls['".$siteURL."'] --><script>window.postMessage({ type: 'monsidoExtension', fn: 'forceRefresh' });</script>"));
 	}
 }
